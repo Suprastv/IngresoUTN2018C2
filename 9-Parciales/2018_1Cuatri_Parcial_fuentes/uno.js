08f8c14a86ca23aac438ef212f687d8265b85be9
Suprastv <40808891+Suprastv@users.noreply.github.com>
2018-07-10 22:15:33 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+var largo;
+var ancho;
+var perimetro;
 
+largo = parseInt(prompt("Ingrese el largo del rectangulo"));
+ancho = parseInt(prompt("Ingrese el ancho del rectangulo"));
+
+perimetro = 2 * largo + ancho;
+
+alert("El perimetro del rectangulo es: " + perimetro);
 }
